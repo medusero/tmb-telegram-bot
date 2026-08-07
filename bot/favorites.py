faa4a2addc5636aa0a21150693c4e286800119b5
medusero <234408661+medusero@users.noreply.github.com>
2026-08-07 08:55:36 +0200

    Añado (tardíamente) archivo con funciones para tratar con guardados

---
+++ bot/favorites.py
@@ -0,0 +1,31 @@
+import json
+from json.decoder import JSONDecodeError
+import os
+
+ugly_path = os.path.join(os.path.dirname(__file__), "..", "data", "favorites.json")
+clean_path = os.path.abspath(ugly_path)
+
+
+def cargar_favoritos():
+    if os.path.isfile(clean_path):
+        try:
+            with open(clean_path, "r") as file:
+                favorites = json.load(file)
+                return favorites
+        except JSONDecodeError:
+            return {}
+    else:
+        favorites = {}
+        return favorites
+
+
+def guardar_favoritos(f):
+    with open(clean_path, "w") as file:
+        json.dump(f, file)
+
+
+def listar_favoritos():
+    favorites = cargar_favoritos()
+    lines = [f"- {key}: {value}" for key, value in favorites.items()]
+    markdown_list = "\n".join(lines)
+    return markdown_list
